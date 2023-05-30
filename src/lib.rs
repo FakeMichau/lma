e037ad6c37fe8072b654a1e5b94b955b1ede75cb
FakeMichau <49685661+FakeMichau@users.noreply.github.com>
2023-05-30 15:19:32 +0200

    Get basic database structure working

---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+use std::collections::HashMap;
+use rusqlite::{Connection, Result};
+
+pub struct AnimeList {
+    db_connection: Connection
+}
+
+impl AnimeList {
+    pub fn get_list(&self) -> Result<HashMap<u64, Show>> {
+        let mut stmt = self.db_connection.prepare("
+            SELECT Shows.id, Shows.progress, Shows.episode_count, Shows.sync_service_id,
+            Episodes.episode_number, Episodes.path
+            FROM Shows
+            JOIN Episodes ON Shows.id = Episodes.show_id;
+        ")?;
+        let mut shows: HashMap<u64, Show> = HashMap::new();
+        let mut rows = stmt.query([])?;
+        while let Some(row) = rows.next()? {
+            let show_id: u64 = row.get(0)?;
+            let progress: u64 = row.get(1)?;
+            let episode_count: u64 = row.get(2)?;
+            let sync_service_id: u64 = row.get(3)?;
+            let episode_number: u64 = row.get(4)?;
+            let path: String = row.get(5)?;
+
+            let show = shows.entry(show_id).or_insert_with(|| Show {
+                progress,
+                episode_count,
+                episodes: HashMap::new(),
+                sync_service_id,
+            });
+            show.episodes.insert(episode_number, path);
+        }
+        Ok(shows)
+    }
+    fn add_entry() {
+
+    }
+    fn remove_entry() {
+        
+    }
+    fn refresh_filesystem() {
+        
+    }
+}
+
+#[derive(Debug)]
+pub struct Show {
+    progress: u64,
+    episode_count: u64,
+    episodes: HashMap<u64, String>,
+    sync_service_id: u64
+}
+
+pub fn create() -> AnimeList {
+    let path = "./database.db3";
+    let db_connection = match Connection::open(path) {
+        Ok(conn) => conn,
+        Err(why) => panic!("Cry - {}", why),
+    };
+    if db_connection.is_readonly(rusqlite::DatabaseName::Main).expect("shouldn't realistically return an error") {
+        panic!("Database is read-only");
+    }
+    match db_connection.execute(
+        "
+        CREATE TABLE Shows (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            progress INTEGER,
+            sync_service_id INTEGER,
+            episode_count INTEGER
+        );
+        ", ()
+    ) {
+            Ok(_) => println!("Table Shows created"),
+            Err(why) => {
+                if why.to_string().contains("already exists") {
+                    println!("Table creation failed: table Shows already exists");
+                } else {
+                    eprintln!("Table creation failed: {}", why.to_string());
+                }
+            }
+    };    
+    match db_connection.execute(
+        "
+        CREATE TABLE Episodes (
+            show_id INTEGER,
+            episode_number INTEGER,
+            path TEXT,
+            PRIMARY KEY (show_id, episode_number),
+            FOREIGN KEY (show_id) REFERENCES Shows(id)
+        );
+        ", ()
+    ) {
+            Ok(_) => println!("Table Episodes created"),
+            Err(why) => {
+                if why.to_string().contains("already exists") {
+                    println!("Table creation failed: table Episodes already exists");
+                } else {
+                    eprintln!("Table creation failed: {}", why.to_string());
+                }
+            }
+    };
+    AnimeList { db_connection }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use lma;
+
 fn main() {
-    println!("Hello, world!");
+    let anime_list = lma::create();
+    let data = anime_list.get_list();
+    if let Ok(result) = data {
+        println!("{:#?}", result);
+    }
 }
