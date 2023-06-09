765e64c078bb1bb36b892f3719197c35c435c6cb
FakeMichau <49685661+FakeMichau@users.noreply.github.com>
2023-06-09 17:31:37 +0200

    Refactor key handling

--- src/app.rs
+++ src/app.rs
@@ -2,18 +2,18 @@ use crate::app;
 use crate::ui::popup::title_selection::TitlesPopup;
 use crate::ui::{
     self,
-    FocusedWindow, SelectionDirection,
     main_menu::StatefulList,
-    popup::insert_show::{InsertPopup, InsertState}
+    popup::insert_show::{InsertPopup, InsertState},
+    FocusedWindow, SelectionDirection,
 };
 use crossterm::event::{self, Event, KeyCode};
-use tokio::runtime::Runtime;
+use ratatui::{backend::Backend, Terminal};
 use std::error::Error;
 use std::{
     io,
     time::{Duration, Instant},
 };
-use ratatui::{backend::Backend, Terminal};
+use tokio::runtime::Runtime;
 
 pub(crate) struct App {
     pub(crate) shows: StatefulList,
@@ -24,7 +24,7 @@ pub(crate) struct App {
 
 impl App {
     pub(crate) fn build(rt: &Runtime) -> Result<App, Box<dyn Error>> {
-        let service = rt.block_on(async {lma::MAL::new().await});
+        let service = rt.block_on(async { lma::MAL::new().await });
         let anime_list = lma::create(service);
         Ok(App {
             shows: StatefulList::with_items(anime_list),
@@ -36,12 +36,55 @@ impl App {
 
     fn generate_test_data(&self) -> bool {
         for i in 1..6 {
-            _=self.shows.items.add_show(format!("Show {}", i).as_str(), 1000 + i, 12*(i%3)+1, 5*(i%3)+1);
-            for e in 1..i+2 {
-                _=self.shows.items.add_episode(i, e, format!("/path/to/episode{}.mp4", e).as_str());
+            _ = self.shows.items.add_show(
+                format!("Show {}", i).as_str(),
+                1000 + i,
+                12 * (i % 3) + 1,
+                5 * (i % 3) + 1,
+            );
+            for e in 1..i + 2 {
+                _ = self.shows.items.add_episode(
+                    i,
+                    e,
+                    format!("/path/to/episode{}.mp4", e).as_str(),
+                );
             }
         }
         true
+    }
+
+    async fn handle_login_popup_async<B: Backend>(
+        &mut self,
+        rt: &Runtime,
+        terminal: &mut Terminal<B>,
+    ) -> io::Result<()> {
+        self.shows.items.service.auth().await;
+        self.focused_window = FocusedWindow::Login;
+        terminal.draw(|f| ui::ui(f, self, rt))?;
+        if self.shows.items.service.get_url().is_some() {
+            self.shows.items.service.login().await; // freezes the app as it waits
+            self.focused_window = FocusedWindow::MainMenu;
+            terminal.draw(|f| ui::ui(f, self, rt)).unwrap();
+            self.focused_window = FocusedWindow::Login;
+        }
+        Ok(())
+    }
+
+    fn handle_login_popup<B: Backend>(
+        &mut self,
+        rt: &Runtime,
+        terminal: &mut Terminal<B>,
+    ) -> io::Result<()> {
+        rt.block_on(self.handle_login_popup_async(rt, terminal))?;
+        Ok(())
+    }
+
+    fn fill_with_api_data(&mut self) {
+        let selected_show = self.titles_popup.selected_show();
+        self.insert_popup.sync_service_id = selected_show.id as i64;
+        self.insert_popup.title = selected_show.title.to_owned(); // make it a config?
+        self.insert_popup.state = InsertState::Next;
+        self.focused_window = FocusedWindow::InsertPopup
     }
 }
 
@@ -49,7 +92,7 @@ pub(crate) fn run<B: Backend>(
     terminal: &mut Terminal<B>,
     mut app: app::App,
     tick_rate: Duration,
-    rt: Runtime
+    rt: Runtime,
 ) -> io::Result<()> {
     let mut last_tick = Instant::now();
     loop {
@@ -61,86 +104,95 @@ pub(crate) fn run<B: Backend>(
         if crossterm::event::poll(timeout)? {
             if let Event::Key(key) = event::read()? {
                 match app.focused_window {
-                    FocusedWindow::MainMenu => match key.code {
-                        KeyCode::Char('q') => return Ok(()),
-                        KeyCode::Down => app.shows.move_selection(SelectionDirection::Next),
-                        KeyCode::Up => app.shows.move_selection(SelectionDirection::Previous),
-                        KeyCode::Right => app.shows.select(),
-                        KeyCode::Left => app.shows.unselect(),
-                        KeyCode::Char('n') => app.focused_window = FocusedWindow::InsertPopup,
-                        KeyCode::Char('l') => {
-                            rt.block_on(async {
-                                app.shows.items.service.auth().await;
-                                app.focused_window = FocusedWindow::Login;
-                                terminal.draw(|f| ui::ui(f, &mut app, &rt)).unwrap();
-                                if app.shows.items.service.get_url().is_some() {
-                                    app.shows.items.service.login().await; // freezes the app as it waits
-                                    app.focused_window = FocusedWindow::MainMenu;
-                                    terminal.draw(|f| ui::ui(f, &mut app, &rt)).unwrap();
-                                    app.focused_window = FocusedWindow::Login;
-                                }
-                            });
-                        },
-                        KeyCode::Char('p') => debug_assert!(app.generate_test_data()),
-                        _ => {}
-                    },
-                    FocusedWindow::InsertPopup => match app.insert_popup.state {
-                        InsertState::Inputting => match key.code {
-                            KeyCode::Char(c) => app.insert_popup.data.push(c),
-                            KeyCode::Backspace => _=app.insert_popup.data.pop(),
-                            KeyCode::Esc => app.insert_popup.state = InsertState::None,
-                            KeyCode::Down => {
-                                app.insert_popup.next_line();
-                                app.insert_popup.state = InsertState::Next;
-                            },
-                            KeyCode::Enter => {
-                                app.insert_popup.state = if app.insert_popup.next_line() {
-                                    InsertState::Save
-                                } else {
-                                    InsertState::Next
-                                };
-                            },
-                            KeyCode::Up => {
-                                app.insert_popup.previous_line();
-                                app.insert_popup.state = InsertState::Next;
-                            },
-                            _ => {}
-                        },
-                        _ => match key.code {
-                            KeyCode::Esc => {
-                                app.focused_window = FocusedWindow::MainMenu;
-                                app.insert_popup = InsertPopup::default()
-                            },
-                            KeyCode::Char('e') => app.insert_popup.state = InsertState::Inputting,
-                            KeyCode::Char('i') => app.insert_popup.state = InsertState::Save,
-                            KeyCode::Down => _=app.insert_popup.next_line(),
-                            KeyCode::Up => app.insert_popup.previous_line(),
-                            _ => {}
-                        },
-                    },
-                    FocusedWindow::Login => match key.code {
-                        KeyCode::Esc => app.focused_window = FocusedWindow::MainMenu,
-                        _ => {}
-                    },
-                    FocusedWindow::TitleSelection => match key.code {
-                        KeyCode::Down => app.titles_popup.move_selection(SelectionDirection::Next),
-                        KeyCode::Up => app.titles_popup.move_selection(SelectionDirection::Previous),
-                        KeyCode::Enter => {
-                            let selected_show = app.titles_popup.selected_show();
-                            app.insert_popup.sync_service_id = selected_show.id as i64;
-                            app.insert_popup.title = selected_show.title.to_owned(); // make it a config?
-                            app.insert_popup.state = InsertState::Next;
-                            app.focused_window = FocusedWindow::InsertPopup
-                        },
-                        KeyCode::Esc => app.focused_window = FocusedWindow::InsertPopup,
-                        _ => {}
-                    },
+                    FocusedWindow::MainMenu => {
+                        if handle_main_menu_key(key, &mut app, &rt, terminal)?.is_none() {
+                            return Ok(());
+                        }
+                    }
+                    FocusedWindow::Login => handle_login_key(key, &mut app),
+                    FocusedWindow::InsertPopup => handle_insert_popup_key(&mut app, key),
+                    FocusedWindow::TitleSelection => handle_title_selection_key(key, &mut app),
                 }
-                
             }
         }
         if last_tick.elapsed() >= tick_rate {
             last_tick = Instant::now();
         }
+    }
+}
+
+fn handle_main_menu_key<B: Backend>(
+    key: event::KeyEvent,
+    app: &mut App,
+    rt: &Runtime,
+    terminal: &mut Terminal<B>,
+) -> Result<Option<bool>, io::Error> {
+    match key.code {
+        KeyCode::Char('q') => return Ok(None),
+        KeyCode::Down => app.shows.move_selection(SelectionDirection::Next),
+        KeyCode::Up => app.shows.move_selection(SelectionDirection::Previous),
+        KeyCode::Right => app.shows.select(),
+        KeyCode::Left => app.shows.unselect(),
+        KeyCode::Char('n') => app.focused_window = FocusedWindow::InsertPopup,
+        KeyCode::Char('l') => app.handle_login_popup(rt, terminal)?,
+        KeyCode::Char('p') => debug_assert!(app.generate_test_data()),
+        _ => {}
+    }
+    return Ok(Some(true));
+}
+
+fn handle_login_key(key: event::KeyEvent, app: &mut App) {
+    match key.code {
+        KeyCode::Esc => app.focused_window = FocusedWindow::MainMenu,
+        _ => {}
+    }
+}
+
+fn handle_insert_popup_key(app: &mut App, key: event::KeyEvent) {
+    match app.insert_popup.state {
+        InsertState::Inputting => match key.code {
+            KeyCode::Char(c) => app.insert_popup.data.push(c),
+            KeyCode::Backspace => _ = app.insert_popup.data.pop(),
+            KeyCode::Esc => app.insert_popup.state = InsertState::None,
+            KeyCode::Enter => {
+                app.insert_popup.state = if app.insert_popup.next_line() {
+                    InsertState::Save
+                } else {
+                    InsertState::Next
+                };
+            }
+            KeyCode::Down => {
+                app.insert_popup.next_line();
+                app.insert_popup.state = InsertState::Next;
+            }
+            KeyCode::Up => {
+                app.insert_popup.previous_line();
+                app.insert_popup.state = InsertState::Next;
+            }
+            _ => {}
+        },
+        _ => match key.code {
+            KeyCode::Esc => {
+                app.focused_window = FocusedWindow::MainMenu;
+                app.insert_popup = InsertPopup::default()
+            }
+            KeyCode::Char('e') => app.insert_popup.state = InsertState::Inputting,
+            KeyCode::Char('i') => app.insert_popup.state = InsertState::Save,
+            KeyCode::Down => _ = app.insert_popup.next_line(),
+            KeyCode::Up => app.insert_popup.previous_line(),
+            _ => {}
+        },
+    }
+}
+
+fn handle_title_selection_key(key: event::KeyEvent, app: &mut App) {
+    match key.code {
+        KeyCode::Down => app.titles_popup.move_selection(SelectionDirection::Next),
+        KeyCode::Up => app
+            .titles_popup
+            .move_selection(SelectionDirection::Previous),
+        KeyCode::Enter => app.fill_with_api_data(),
+        KeyCode::Esc => app.focused_window = FocusedWindow::InsertPopup,
+        _ => {}
     }
 }
