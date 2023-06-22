c8b77863b7b95921697f57bb63070c464e03e256
FakeMichau <49685661+FakeMichau@users.noreply.github.com>
2023-06-22 22:12:35 +0200

    Test for local service

--- src/api.rs
+++ src/api.rs
@@ -10,12 +10,13 @@ pub enum ServiceType {
     Local
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub struct ServiceTitle {
     pub service_id: u32,
     pub title: String,
 }
 
+#[derive(PartialEq, Eq, Debug)]
 pub struct ServiceEpisodeUser {
     pub status: Option<EpisodeStatus>,
     pub progress: Option<u32>,
@@ -28,6 +29,7 @@ pub struct ServiceEpisodeUser {
     pub comments: Option<String>,
 }
 
+#[derive(PartialEq, Eq, Debug)]
 pub enum EpisodeStatus {
     None,
     Watching,
@@ -37,6 +39,7 @@ pub enum EpisodeStatus {
     PlanToWatch,
 }
 
+#[derive(PartialEq, Eq, Debug)]
 pub struct ServiceEpisodeDetails {
     pub number: Option<u32>,
     pub title: Option<String>,
