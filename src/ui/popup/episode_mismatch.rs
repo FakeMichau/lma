1da535356b9d2812ef3a17146b0d71c291b55039
FakeMichau <49685661+FakeMichau@users.noreply.github.com>
2023-06-23 16:54:19 +0200

    Episode mismatch

--- src/ui/popup/episode_mismatch.rs
+++ src/ui/popup/episode_mismatch.rs
@@ -53,22 +53,25 @@ impl MismatchPopup {
                     let mut range = slice.split('-');
                     let min = range
                         .next()
+                        .and_then(|str| if str.is_empty() {None} else {Some(str)})
                         .ok_or("Can't find minimum value from the range")?
                         .trim()
                         .parse::<u32>()
                         .map_err(|err| format!("Value from range must be a number: {err}"))?;
                     let max = range
                         .next()
+                        .and_then(|str| if str.is_empty() {None} else {Some(str)})
                         .ok_or("Can't find maximum value from the range")?
                         .trim()
                         .parse::<u32>()
-                        .map_err(|err| format!("Value from range must be a number {err}"))?;
+                        .map_err(|err| format!("Value from range must be a number: {err}"))?;
                     (min..=max).for_each(|value| episodes_from_slice.push(value));
                 } else {
                     let episode = slice.trim();
                     if !episode.is_empty() {
                         episodes_from_slice
-                            .push(episode.parse::<u32>().expect("Episode must be a number"));
+                            .push(episode.parse::<u32>()
+                                .map_err(|err| format!("Episode must be a number: {err}"))?);
                     }
                 }
                 Ok(episodes_from_slice)
@@ -132,4 +135,89 @@ pub fn build<B: Backend, T: Service>(frame: &mut Frame<B>, app: &mut App<T>) {
     frame.render_widget(block, area);
     frame.render_widget(mismatch_info, main_chunks[0]);
     frame.render_widget(user_input, main_chunks[1]);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_individual() {
+        let mut mismatch_popup = MismatchPopup::new(15, 14);
+
+        mismatch_popup.owned_episodes = String::from("1, 3, 5, 7");
+        let result = mismatch_popup.parse_owned();
+        assert!(result.is_ok());
+        let parsed_owned = result.unwrap();
+        let expected_owned: BTreeSet<u32> = vec![1, 3, 5, 7].into_iter().collect();
+        assert_eq!(parsed_owned, expected_owned);
+    }
+
+    #[test]
+    fn parse_range() {
+        let mut mismatch_popup = MismatchPopup::new(15, 14);
+
+        mismatch_popup.owned_episodes = String::from("1-5, 8-10");
+        let result = mismatch_popup.parse_owned();
+        assert!(result.is_ok());
+        let parsed_owned = result.unwrap();
+        let expected_owned: BTreeSet<u32> = vec![1, 2, 3, 4, 5, 8, 9, 10].into_iter().collect();
+        assert_eq!(parsed_owned, expected_owned);
+    }
+
+    #[test]
+    fn parse_combo() {
+        let mut mismatch_popup = MismatchPopup::new(15, 14);
+
+        mismatch_popup.owned_episodes = String::from("1-4, 6, 8-10, 12");
+        let result = mismatch_popup.parse_owned();
+        assert!(result.is_ok());
+        let parsed_owned = result.unwrap();
+        let expected_owned: BTreeSet<u32> = vec![1, 2, 3, 4, 6, 8, 9, 10, 12].into_iter().collect();
+        assert_eq!(parsed_owned, expected_owned);
+    }
+
+    #[test]
+    fn parse_range_no_max() {
+        let mut mismatch_popup = MismatchPopup::new(15, 14);
+
+        mismatch_popup.owned_episodes = String::from("1-");
+        let result = mismatch_popup.parse_owned();
+        assert!(result.is_err());
+        let error = result.err().unwrap();
+        assert_eq!(error, "Can't find maximum value from the range");
+    }
+
+    #[test]
+    fn parse_range_no_min() {
+        let mut mismatch_popup = MismatchPopup::new(15, 14);
+
+        mismatch_popup.owned_episodes = String::from("-5");
+        let result = mismatch_popup.parse_owned();
+        assert!(result.is_err());
+        let error = result.err().unwrap();
+        assert_eq!(error, "Can't find minimum value from the range");
+    }
+
+    #[test]
+    fn parse_range_nan() {
+        let mut mismatch_popup = MismatchPopup::new(15, 14);
+
+        mismatch_popup.owned_episodes = String::from("3-sd");
+        let result = mismatch_popup.parse_owned();
+        assert!(result.is_err());
+        let error = result.err().unwrap();
+        assert_eq!(error, "Value from range must be a number: invalid digit found in string");
+    }
+
+    #[test]
+    fn parse_individual_nan() {
+        let mut mismatch_popup = MismatchPopup::new(15, 14);
+
+        mismatch_popup.owned_episodes = String::from("1, abc, 3");
+        let result = mismatch_popup.parse_owned();
+        assert!(result.is_err());
+        let error = result.err().unwrap();
+        assert_eq!(error, "Episode must be a number: invalid digit found in string");
+    }
 }
